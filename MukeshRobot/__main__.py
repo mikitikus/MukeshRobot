413df4c5ed722ec4b35327f0fa2ddf411f3c7e1d
mikitikus <149916893+mikitikus@users.noreply.github.com>
2024-02-18 19:11:19 +0800

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -216,9 +216,9 @@ def start(update: Update, context: CallbackContext):
                 PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN
             )
             time.sleep(0.4)
-            lol.edit_text("❤")
+            lol.edit_text("😘")
             time.sleep(0.5)
-            lol.edit_text("⚡")
+            lol.edit_text("🥰")
             time.sleep(0.3)
             lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ... ")
             time.sleep(0.4)
